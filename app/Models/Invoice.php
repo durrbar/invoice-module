6c684d1a3091f32618c522e382a807fb45c4b3b5
Kid Max <kidmax285@gmail.com>
2025-02-15 22:21:13 +0600

    register the observer

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,13 +2,16 @@
 
 namespace Modules\Invoice\Models;
 
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Modules\Invoice\Observers\InvoiceObserver;
 
 // use Modules\Invoice\Database\Factories\InvoiceFactory;
 
+#[ObservedBy([InvoiceObserver::class])]
 class Invoice extends Model
 {
     use HasFactory;
